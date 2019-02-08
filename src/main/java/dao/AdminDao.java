b4aeebbbd3e787c0d03b5f8341852a994d755968
Anna Beketova <anyamb@yandex.ru>
2019-02-08 20:23:59 +0300

    AdminDao without Admin getByLogin(String login); added PointDao without List<Point> getByBizType(long bizTypeId); added AbstractDao<T> added all DistrictDao added all BizTypeDao added all
    Entities District, Point, BizType, Admin added all

---
+++ src/main/java/dao/AdminDao.java
@@ -0,0 +1,18 @@
+//package dao;
+//
+//import entity.Admin;
+//import org.hibernate.SessionFactory;
+//
+//
+//public class AdminDao extends AbstractDao<Admin>{
+//
+//
+//    public AdminDao(SessionFactory sessionFactory) {
+//        super(sessionFactory);
+//    }
+//
+////    public Admin getByLogin(String login) {
+////        sessionFactory.openSession().createQuery("FROM admin WHERE")
+////    }
+//    //Admin getByLogin(String login);
+//}
---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @Column(name = "id", unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    public Admin() {}
+
+    public Admin(String login, String password) {
+        this.setLogin(login);
+        this.setPassword(password);
+    }
+
+//    public Admin(long id, String login, String password) {
+//        this.setId(id);
+//        this.setLogin(login);
+//        this.setPassword(password);
+//    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
