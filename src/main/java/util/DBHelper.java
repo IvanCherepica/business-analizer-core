9724b13f06b32d17de90fac65f4340d7ce5cc28f
APerev <perev13@gmail.com>
2019-02-09 22:22:17 +0300

    Add AdminService, AdminServiceImpl, IndexPageServlet, LoginServlet, login.jsp, user.jsp. Delete PropertiesReader. Update DBHelper.

--- src/main/java/util/DBHelper.java
+++ src/main/java/util/DBHelper.java
@@ -1,7 +1,10 @@
 package util;
 
 import model.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class DBHelper {
 
@@ -12,13 +15,20 @@ public class DBHelper {
 		configuration.addAnnotatedClass(District.class);
 		configuration.addAnnotatedClass(Point.class);
 
-		configuration.setProperty("hibernate.dialect", PropertiesReader.getProperties("dialect"));
-		configuration.setProperty("hibernate.connection.driver_class", PropertiesReader.getProperties("driver.class"));
-		configuration.setProperty("hibernate.connection.url", PropertiesReader.getProperties("connection.url"));
-		configuration.setProperty("hibernate.connection.username", PropertiesReader.getProperties("username"));
-		configuration.setProperty("hibernate.connection.password", PropertiesReader.getProperties("password"));
-		configuration.setProperty("hibernate.show_sql", PropertiesReader.getProperties("show_sql"));
-		configuration.setProperty("hibernate.hbm2ddl.auto", PropertiesReader.getProperties("hbm2ddl.auto"));
+		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+		configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/db_schema");
+		configuration.setProperty("hibernate.connection.username", "root");
+		configuration.setProperty("hibernate.connection.password", "admin");
+		configuration.setProperty("hibernate.show_sql", "true");
+		configuration.setProperty("hibernate.hbm2ddl.auto", "update");
 		return configuration;
+	}
+
+	static public SessionFactory createSessionFactory(Configuration configuration) {
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+		builder.applySettings(configuration.getProperties());
+		ServiceRegistry serviceRegistry = builder.build();
+		return configuration.buildSessionFactory(serviceRegistry);
 	}
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>login</title>
+</head>
+<body>
+<form action="/login" method="POST">
+    Login: <input type="text" name="login"/>
+    Password: <input type="password" name="password"/>
+    <input type="submit" value="Sign in">
+</form>
+</body>
+</html>
