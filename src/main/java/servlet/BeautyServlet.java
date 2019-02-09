99cfeb833827f5b5f6591162c7decd9731b29a4a
VSemenenko <47300955+VSemenenko@users.noreply.github.com>
2019-02-10 01:27:57 +0300

    Создал страницу админа

--- src/main/java/servlet/BeautyServlet.java
+++ src/main/java/servlet/BeautyServlet.java
@@ -9,17 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
+@WebServlet("/admin/beauty")
+public class BeautyServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html utf-8");
+        List<Point> points = new LinkedList<>();
+        points.add(new Point("Crop", "ул. Чехова, 9",59915585,30317665,3));
+        points.add(new Point("12 месяцев", "Владимирский просп., 19, ТК Владимирский пассаж, эт. 4",59916293, 30320225,3 ));
+        points.add(new Point("Waxabar", "ул. Марата, 77",59914669, 30321186,3 ));
+        points.add(new Point("You New","Итальянская ул., 31",59908638, 30322031,3 ));
+        points.add(new Point("Chic Nail","ул. Жуковского, 14",59936341, 30322866,3 ));
+        points.add(new Point("Галька и галыш", "Гродненский пер., 12-14",59934592, 30334778,2 ));
+        request.setAttribute("points", points);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/beauty.jsp");
+        dispatcher.forward(request, response);
+    }
 
-@WebServlet("/admin")
-public class AdminServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html utf-8");
-        List <Point> points = new LinkedList<>();
+        List<Point> points = new LinkedList<>();
         points.add(new Point("Озерки","Московский просп., 25/1",59915585,30317665,2));
         points.add(new Point("ГорЗдрав","Бронницкая ул., 5",59916293, 30320225,2 ));
         points.add(new Point("Софосбувир СПб", "Клинский просп., 21",59914669, 30321186,2 ));
@@ -27,11 +39,7 @@ public class AdminServlet extends HttpServlet {
         points.add(new Point("Петрофарм","Большая Конюшенная ул., 14",59936341, 30322866,2 ));
         points.add(new Point("Петербургские аптеки", "Невский просп., 50",59934592, 30334778,2 ));
         request.setAttribute("points", points);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/beauty.jsp");
         dispatcher.forward(request, response);
-
-
-
-
     }
 }
--- src/main/java/servlet/СlothesServlet.java
+++ src/main/java/servlet/СlothesServlet.java
@@ -9,17 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-
-@WebServlet("/admin")
-public class AdminServlet extends HttpServlet {
+@WebServlet("/admin/clothes")
+public class СlothesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html utf-8");
-        List <Point> points = new LinkedList<>();
+        List<Point> points = new LinkedList<>();
         points.add(new Point("Озерки","Московский просп., 25/1",59915585,30317665,2));
         points.add(new Point("ГорЗдрав","Бронницкая ул., 5",59916293, 30320225,2 ));
         points.add(new Point("Софосбувир СПб", "Клинский просп., 21",59914669, 30321186,2 ));
@@ -27,11 +24,7 @@ public class AdminServlet extends HttpServlet {
         points.add(new Point("Петрофарм","Большая Конюшенная ул., 14",59936341, 30322866,2 ));
         points.add(new Point("Петербургские аптеки", "Невский просп., 50",59934592, 30334778,2 ));
         request.setAttribute("points", points);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/clothes.jsp");
         dispatcher.forward(request, response);
-
-
-
-
     }
 }
