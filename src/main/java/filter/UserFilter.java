22490f618c322ac804a8115651c9326092ce92ed
VSemenenko <47300955+VSemenenko@users.noreply.github.com>
2019-02-11 13:50:49 +0300

    удален лишние срвлеты +  раскоментированли код

--- src/main/java/filter/UserFilter.java
+++ src/main/java/filter/UserFilter.java
@@ -1,15 +1,26 @@
-//package filter;
-//
-//import model.User;
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//
-//@WebFilter(urlPatterns = {"/user"})
-//public class UserFilter implements Filter {
-//
-//}
+package filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/user"})
+public class UserFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
