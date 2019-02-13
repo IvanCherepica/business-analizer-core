999ad73f977d97d0e55fbfd15b1f2d187dc965fb
Anna Beketova <anyamb@yandex.ru>
2019-02-13 17:03:27 +0300

    color gradient added

--- src/main/webapp/js/mapSupport.js
+++ src/main/webapp/js/mapSupport.js
@@ -1,22 +1,67 @@
 ymaps.ready(init);
 
-var opacity_for_all = 0.6;
+var opacity_for_all = 0.7;
 
 function getColor(num) {
-    var resultColor = '#FF0000';
-    if (num > 0.8) {
-        resultColor = '#FF0000';
-    } else if (num > 0.6) {
-        resultColor = '#ff7f2e';
-    } else if (num > 0.4) {
-        resultColor = '#ffd31b';
-    } else if (num > 0.2) {
-        resultColor = '#b3ff3a';
-    } else {
-        resultColor = '#00FF00';
-    }
-    return resultColor;
+    return getGradientColor('#00FF00', '#FF0000', num);
 }
+
+getGradientColor = function(start_color, end_color, percent) {
+    // strip the leading # if it's there
+    start_color = start_color.replace(/^\s*#|\s*$/g, '');
+    end_color = end_color.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(start_color.length == 3){
+        start_color = start_color.replace(/(.)/g, '$1$1');
+    }
+
+    if(end_color.length == 3){
+        end_color = end_color.replace(/(.)/g, '$1$1');
+    }
+
+    // get colors
+    var start_red = parseInt(start_color.substr(0, 2), 16),
+        start_green = parseInt(start_color.substr(2, 2), 16),
+        start_blue = parseInt(start_color.substr(4, 2), 16);
+
+    var end_red = parseInt(end_color.substr(0, 2), 16),
+        end_green = parseInt(end_color.substr(2, 2), 16),
+        end_blue = parseInt(end_color.substr(4, 2), 16);
+
+    // calculate new color
+    var diff_red = end_red - start_red;
+    var diff_green = end_green - start_green;
+    var diff_blue = end_blue - start_blue;
+
+    diff_red = ( (diff_red * percent) + start_red ).toString(16).split('.')[0];
+    diff_green = ( (diff_green * percent) + start_green ).toString(16).split('.')[0];
+    diff_blue = ( (diff_blue * percent) + start_blue ).toString(16).split('.')[0];
+
+    // ensure 2 digits by color
+    if( diff_red.length == 1 ) diff_red = '0' + diff_red
+    if( diff_green.length == 1 ) diff_green = '0' + diff_green
+    if( diff_blue.length == 1 ) diff_blue = '0' + diff_blue
+
+    return '#' + diff_red + diff_green + diff_blue;
+};
+
+// function getColor(num) {
+//     var resultColor = '#FF0000';
+//     if (num > 0.8) {
+//         resultColor = '#FF0000';
+//     } else if (num > 0.6) {
+//         resultColor = '#ff7f2e';
+//     } else if (num > 0.4) {
+//         resultColor = '#ffd31b';
+//     } else if (num > 0.2) {
+//         resultColor = '#b3ff3a';
+//     } else {
+//         resultColor = '#00FF00';
+//     }
+//     return resultColor;
+// }
+
 
 function calculateColor(innerListOfNumberOfElements) {
     var maxNum = Math.max.apply(1, innerListOfNumberOfElements);
