2af9e49d90d0c19b770a0f6ca67561af263124ac
APerev <perev13@gmail.com>
2019-02-14 18:31:54 +0300

    Add SessionFactoryHolderSingleton class, fix spelling mistakes

--- src/main/java/util/SessionFactoryHolderSingleton.java
+++ src/main/java/util/SessionFactoryHolderSingleton.java
@@ -7,15 +7,21 @@ import static util.DBHelper.createSessionFactory;
 
 public class SessionFactoryHolderSingleton {
     private static SessionFactory sessionFactory;
-    private static SessionFactoryHolderSingleton instance;
+    private static volatile  SessionFactoryHolderSingleton instance;
 
     private SessionFactoryHolderSingleton(Configuration configuration) {
         this.sessionFactory = createSessionFactory(configuration);
     }
 
     public static SessionFactory getSessionInstance(Configuration configuration) {
-        if (instance == null) {
-            instance = new SessionFactoryHolderSingleton(configuration);
+        SessionFactoryHolderSingleton localInstance = instance;
+        if (localInstance == null) {
+            synchronized (SessionFactoryHolderSingleton.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new SessionFactoryHolderSingleton(configuration);
+                }
+            }
         }
         return sessionFactory;
     }
