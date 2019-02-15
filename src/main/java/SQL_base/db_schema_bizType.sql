47e6c42b1d1c4026428ef14542e8a2ee66fc0d2e
Anna Beketova <anyamb@yandex.ru>
2019-02-15 23:46:35 +0300

    added proper SQL databases

--- src/main/java/SQL_base/db_schema_bizType.sql
+++ src/main/java/SQL_base/db_schema_bizType.sql
@@ -3,12 +3,13 @@ create table biztype
   id         bigint auto_increment
     primary key,
   name       varchar(255)  null,
-  searchTags varchar(1000) null
+  searchTags varchar(1000) null,
+  link       varchar(255)  null
 )
   charset = utf8;
 
-INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (1, 'Кафе', 'кафе | рестораны', '/cafe_icon.png');
+INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (1, 'Фитнес клуб', 'фитнес клуб', '/exercise_icon.png');
 INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (2, 'Аптеки', 'аптека', '/drugs_icon.png');
-INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (3, 'Салоны красоты', 'салоны красоты | барбер шоп | парикмахерские | спа | маникюр | студия красоты | солярий', '/beauty_icon.png');
-INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (4, 'Продуктовые магазины', 'магазин | продукты', '/food_icon.png');
+INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (3, 'Салоны красоты', 'салон красоты', '/beauty_icon.png');
+INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (4, 'Продуктовые магазины', 'продукты', '/food_icon.png');
 INSERT INTO db_schema.biztype (id, name, searchTags, link) VALUES (5, 'Магазины одежды', 'одежда', '/clothes_icon.png');
