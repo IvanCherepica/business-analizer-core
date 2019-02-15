9e87eab783e2c9e05de57599995a036db2170ef9
APerev <perev13@gmail.com>
2019-02-15 17:09:58 +0300

    Add deleting points while deleting ByzType

--- src/main/java/servlet/DeleteBizTypeServlet.java
+++ src/main/java/servlet/DeleteBizTypeServlet.java
@@ -1,12 +1,15 @@
 package servlet;
 
+import model.Point;
 import service.BizTypeServiceImpl;
+import service.PointServiceImpl;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/admin/business/delete")
 public class DeleteBizTypeServlet extends HttpServlet {
@@ -17,6 +20,12 @@ public class DeleteBizTypeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         long id = Long.parseLong(request.getParameter("id"));
+
+        //Удаляем список поинтов по данному типу bizType.id
+        PointServiceImpl pointService = new PointServiceImpl();
+        List<Point> pointListToRemove = pointService.getByBizType(id);
+        pointService.removeList(pointListToRemove);
+
         bizTypeService.remove(id);
 
         response.sendRedirect("/admin/business");
